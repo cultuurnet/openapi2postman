55cac50ce204e926f60053d2cb964cd6153126b2
Bert Ramakers <bertramakers@me.com>
2021-10-29 11:16:36 +0200

    Stop if the schema could not be dereferenced

--- bin/index.js
+++ bin/index.js
@@ -7,10 +7,12 @@ const openApiSchemaFile = './entry.json'; // Hardcoded for now
 (
   async () => {
     try {
-      let deReferenced = await $RefParser.dereference(openApiSchemaFile);
-      console.log(JSON.stringify(deReferenced, null, 2));
+      // Dereference the OpenAPI schema to resolve $refs to schemas in other files.
+      const deReferencedOpenApiSchema = await $RefParser.dereference(openApiSchemaFile);
+      console.log(JSON.stringify(deReferencedOpenApiSchema, null, 2));
     } catch(err) {
       console.error(err);
+      process.exit(1);
     }
   }
 )();
