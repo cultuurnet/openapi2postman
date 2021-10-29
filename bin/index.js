459943985ca84db02b0fab51f2381807ac99f42a
Bert Ramakers <bertramakers@me.com>
2021-10-29 17:01:09 +0200

    Work with arguments/options to configure script

--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,70 @@
 #!/usr/bin/env node
 
+const yargs = require('yargs');
 const convert = require('../convert.js');
 const fs = require('fs');
 
-const openApiSchemaFile = './entry.json';
-const postmanCollectionFile = './entry-postman.json';
+const convertWithArgv = (argv) => {
+  const {openApiSchemaFile, environment} = argv;
+  const authOptions = {
+    tokenGrantType: argv.tokenGrantType,
+    clientId: argv.clientId,
+    clientSecret: argv.clientSecret
+  };
 
-const environment = 'test';
+  if (authOptions.tokenGrantType === 'authorization_code') {
+    const {userAuthCallbackUrl} = argv;
+    authOptions.callbackUrl = userAuthCallbackUrl;
+  }
 
-const authOptions = {
-  tokenGrantType: 'client_credentials',
-  clientId: 'mock',
-  clientSecret: 'mock',
-  callbackUrl: 'https://jwt-test.uitdatabank.be/authorize'
+  convert(openApiSchemaFile, environment, authOptions).then((postmanCollection) => {
+    console.log('Writing Postman v2.1 collection to file...');
+    const postmanCollectionFile = './entry-postman.json';
+    fs.writeFileSync(postmanCollectionFile, JSON.stringify(postmanCollection, null, 2));
+    console.log('Wrote Postman v2.1 collection to file!');
+  });
 }
 
-convert(openApiSchemaFile, environment, authOptions).then((postmanCollection) => {
-  console.log('Writing Postman v2.1 collection to file...');
-  fs.writeFileSync(postmanCollectionFile, JSON.stringify(postmanCollection, null, 2));
-  console.log('Wrote Postman v2.1 collection to file!');
-});
+yargs
+  .scriptName("openapi2postman")
+  .usage('$0 <cmd> [args]')
+  .command(
+    'convert <openApiSchemaFile> <clientId> <clientSecret> [environment] [tokenGrantType] [userAuthCallbackUrl]',
+    'Convert an OpenAPI schema file to a Postman collection and configure it for integrators',
+    (yargs) => {
+      yargs.positional('openApiSchemaFile', {
+        type: 'string',
+        describe: 'the OpenAPI file to convert'
+      });
+      yargs.positional('clientId', {
+        type: 'string',
+        describe: 'the client\'s id to use for authentication'
+      });
+      yargs.positional('clientSecret', {
+        type: 'string',
+        describe: 'the client\'s secret to use for authentication'
+      });
+      yargs.option('environment', {
+        alias: 'e',
+        default: 'test',
+        choices: ['acc', 'test', 'prod'],
+        describe: 'the API\'s server environment to use as base URL for all requests',
+        type: 'string'
+      });
+      yargs.option('tokenGrantType', {
+        alias: 'g',
+        default: 'client_credentials',
+        choices: ['client_credentials', 'authorization_code'],
+        describe: 'the grant type to use to get tokens from the authorization server',
+        type: 'string'
+      });
+      yargs.option('userAuthCallbackUrl', {
+        default: '',
+        describe: 'the callback url to use when using the authorization_code grant type',
+        type: 'string'
+      });
+    },
+    convertWithArgv
+  )
+  .help()
+  .argv;
